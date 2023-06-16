9f0b165fa151669e378268f8b2364d21dec074a9
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-06-16 14:00:26 -0300

    Criado banco de dados Clientes

--- aulaGit/models.py
+++ aulaGit/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=255)
+    email = models.TextField(max_length=255)
+    senha = models.TextField(max_length=255)
+    telefone = models.TextField(max_length=255)
+    
