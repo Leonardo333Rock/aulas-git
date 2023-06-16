c2884e2a02902b3c18ba112cb6268cd9b438c3d6
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-06-16 16:22:39 -0300

    Conecçao com o banco Clientes criado

--- aulaGit/urls.py
+++ aulaGit/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.Home, name='home'),
-    path('pagina_de_cadastro', views.Pagina_de_cadastro,name='pg_de-cadastro')
+    path('pagina_de_cadastro', views.Pagina_de_cadastro,name='pg_de-cadastro'),
+    path('cadastro_sucesso',views.cadastro_sucesso,name="cadastro_sucesso")
 ]
--- aulaGit/views.py
+++ aulaGit/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from . models import Cliente
 
 def Home(request):
     return render(request,'home.html')
 
 
 def Pagina_de_cadastro(request):
+    return render(request,'paginas/pg_de_dacastro.html')
 
+def cadastro_sucesso(request):
+    cliente = Cliente()
+    cliente.nome = request.POST.get('nome')
+    cliente.email = request.POST.get('email')
+    cliente.senha = request.POST.get('senha')
+    cliente.telefone = request.POST.get('telefone')
+    cliente.save()
     return render(request,'paginas/pg_de_dacastro.html')
