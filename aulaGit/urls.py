c84cd0f8a488c7c62f6cacfe4e91ac242418927b
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-06-16 14:46:03 -0300

    pagina de cadastro criada

--- aulaGit/urls.py
+++ aulaGit/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('',views.Home, name='home'),
+    path('pagina_de_cadastro', views.Pagina_de_cadastro,name='pg_de-cadastro')
 ]
--- aulaGit/views.py
+++ aulaGit/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def Home(request):
     return render(request,'home.html')
+
+
+def Pagina_de_cadastro(request):
+
+    return render(request,'paginas/pg_de_dacastro.html')
