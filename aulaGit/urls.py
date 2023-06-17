fb03176745c8af1d0c9b38d60243686f0b0b6e9e
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-06-17 08:36:37 -0300

    casual

--- aulaGit/urls.py
+++ aulaGit/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.Home, name='home'),
     path('pagina_de_cadastro', views.Pagina_de_cadastro,name='pg_de-cadastro'),
-    path('cadastro_sucesso',views.cadastro_sucesso,name="cadastro_sucesso")
+    path('cadastro_sucesso',views.cadastro_sucesso,name="cadastro_sucesso"),
+
 ]
