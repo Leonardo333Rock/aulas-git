0fe4a754d35ccee898894b5867fc4e6733100794
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-06-19 15:15:20 -0300

    Pagina produtos.html criada

--- aulaGit/urls.py
+++ aulaGit/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('cadastro_sucesso',views.cadastro_sucesso,name="cadastro_sucesso"),
     path('editar_cadastro/<int:id>',views.editar_cadastro,name="editar_cadastro"),
     path('editado_sucesso',views.editado_sucesso,name="editado_sucesso"),
+    path('produtos',views.produtos,name='produtos')
 
 ]
