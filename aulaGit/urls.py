adf593ec18b40ef7b0cce1d84abd3d78a62a69b7
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-06-19 13:06:43 -0300

    Criada pagina de alteraçao de cadastro

--- aulaGit/urls.py
+++ aulaGit/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.Home, name='home'),
     path('pagina_de_cadastro', views.Pagina_de_cadastro,name='pg_de-cadastro'),
-    path('cadastro_sucesso',views.cadastro_sucesso,name="cadastro_sucesso")
+    path('cadastro_sucesso',views.cadastro_sucesso,name="cadastro_sucesso"),
+    path('editar_cadastro/<int:id>',views.editar_cadastro,name="editar_cadastro"),
+    path('editado_sucesso',views.editado_sucesso,name="editado_sucesso"),
+
 ]
