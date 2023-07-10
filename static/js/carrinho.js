e8a47813b81fff2fdd5cfd15175f5cf0ba944287
LeonardoMatheus <onardo_mateus@hotmail.com>
2023-07-10 11:28:09 -0300

    modificado o carrinho

--- static/js/carrinho.js
+++ static/js/carrinho.js
@@ -2,21 +2,28 @@ var produtos = JSON.parse(localStorage.getItem('produto'))
 let produto = document.getElementById('produto')
 let valor_uni = document.getElementById('valor-uni')
 let caixa = document.getElementById('caixa')
+let valor_total = document.getElementById('valor_total')
 
+let vl_total = 0
 
+function formatarValorUSDparaBRL(valorUSD) {
+    const valorBRL = valorUSD.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return valorBRL;
+  }
 
 produtos.map((e)=>{
     novo = document.createElement('div')
-    novo.setAttribute('class','container-fluid row')
+    novo.setAttribute('class','container-fluid row border-dark')
     p_pdt = document.createElement('p')
-    p_pdt.setAttribute('class','col h6 border')
+    p_pdt.setAttribute('class','col h6 border border-dark')
     p_pdt.innerHTML= e['produto']
 
     p_val = document.createElement('p')
-    p_val.setAttribute('class','col h6 border')
+    p_val.setAttribute('class','col h6 border border-dark')
     p_val.innerHTML =  e['valor']
 
-
+    vl_total += parseFloat(e['valor'])
+    valor_total.innerHTML = formatarValorUSDparaBRL(vl_total)
     caixa.appendChild(novo)
     novo.appendChild(p_pdt)
     novo.appendChild(p_val)
